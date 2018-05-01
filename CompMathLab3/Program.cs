4dc01054d0d44bca95df35e39cb675954a3e50a5
Unknown <sheremet5555@gmail.com>
2018-05-02 01:36:00 +0300

    lab 3 & 5

---
+++ CompMathLab3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompMathLab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, double> func = x => 1 / (1.3 + 2.4 * x);
+
+            Console.WriteLine(SquareCalculator.SimpsonMethod(func, 1, 2.6));
+        }
+    }
+}
---
+++ CompMathLab3/SquareCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompMathLab3
+{
+    static class SquareCalculator
+    {
+        public static double SimpsonMethod(Func<double, double> func, double left, double right)
+        {
+            const double h = 0.2;
+
+            double funcEven = 0;
+            double funcOdd = 0;
+
+            for (double i = left + 2 * h; i < right; i += 2 * h)
+            {
+                funcEven += func(i);
+            }
+
+            for (double i = left + h; i < right; i += 2 * h)
+            {
+                funcOdd += func(left + i * h);
+            }
+
+            return h / 3 * (func(left) + 2 * funcEven + 4 * funcOdd + func(right));
+        }
+
+        public static double ErrorEstimation(Func<double, double> func, double left, double right)
+        {
+            return 0;
+        }
+    }
+}
